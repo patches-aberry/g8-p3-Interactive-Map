9db9d6d38d1e0a5a510f9061e1f7da74e2f4301e
Patrick Aubry <patrickdaubry@gmail.com>
2023-06-04 10:43:47 -0600

    working on js

--- js/tables-logic.js
+++ js/tables-logic.js
@@ -1,4 +1,4 @@
-var states = document.getElementsByName("states")
+var states = document.getElementById("states-multi-select")
 
 states.addEventListener("click", function(){
     var options = states.querySelectorAll("option")
